650f814534f029cf78a7c73c32e7ac55afb9bd90
nasarabu <abunasar388@gmail.com>
2020-10-09 08:54:18 +0530

    Updates

---
+++ precache-manifest.948f57007abe17fed2b2cf69aa7a3e70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c436839ac20f2bfa9ce8b3df73978a12",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "d4c5b502f94d2c6278f3",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aea8f2083db3efac77d0",
+    "url": "/smart-brain/static/css/main.ffead349.chunk.css"
+  },
+  {
+    "revision": "d4c5b502f94d2c6278f3",
+    "url": "/smart-brain/static/js/2.08d847b3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.08d847b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea8f2083db3efac77d0",
+    "url": "/smart-brain/static/js/main.a96de23e.chunk.js"
+  },
+  {
+    "revision": "257db6b14da30691b4a8",
+    "url": "/smart-brain/static/js/runtime-main.1bac4abf.js"
+  }
+]);
